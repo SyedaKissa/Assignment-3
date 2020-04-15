1180be73482b928069859dec6f74b001d1223a31
SyedaKissa <60823225+SyedaKissa@users.noreply.github.com>
2020-04-15 19:58:04 +0500

    Create a3t2.c

---
+++ a3t2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>          // printf(), scanf()               
+#include <stdlib.h>         // exit(), malloc(), free(), rand()
+#include <sys/types.h>      // key_t, sem_t, pid_t      
+#include <sys/shm.h>        // shmat(), IPC_RMID in shmctl()      
+#include <errno.h>          // errno, ECHILD            
+#include <semaphore.h>      // sem_open(), sem_destroy(), sem_wait().. 
+#include <fcntl.h>          // O_CREAT, O_EXEC 
+
+int main(){
+	return 0;
+}
