1e5deb222b99bc6549e1177510740c97c2880458
SyedaKissa <60823225+SyedaKissa@users.noreply.github.com>
2020-04-15 16:19:01 +0500

    Update ass3.c

--- ass3.c
+++ ass3.c
@@ -1,10 +1,13 @@
-#include <stdio.h>       //For c lang
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/shm.h>     //For shared memory
-#include <errno.h>
-#include <semaphore.h>   //For sem_wait and sem_post
-#include <fcntl.h>
+#include <stdio.h>          // printf(), scanf()               
+#include <stdlib.h>         // exit(), malloc(), free(), rand()
+#include <sys/types.h>      // key_t, sem_t, pid_t      
+#include <sys/shm.h>        // shmat(), IPC_RMID in shmctl()      
+#include <errno.h>          // errno, ECHILD            
+#include <semaphore.h>      // sem_open(), sem_destroy(), sem_wait().. 
+#include <fcntl.h>          // O_CREAT, O_EXEC 
+
+
+
 
 sem_t* potentialCPatients_sem;
 sem_t* coronaPatient;
